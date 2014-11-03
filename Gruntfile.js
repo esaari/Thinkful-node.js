9c96dd778b8879c881df5ebadedc478c28d089d3
Eric Saari <esaari@shutterstock.com>
2014-11-02 23:14:42 -0500

    files created after running grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,15 +8,47 @@ module.exports = function(grunt) {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
             },
             build: {
-                src: 'src/<%= pkg.name %>.js',
-                dest: 'build/<%= pkg.name %>.min.js'
+                src: 'public/javascripts/my.js',
+                dest: 'public/javascripts/my.min.js'
+            }
+        },
+        jslint: { // configure the task
+            // lint your project's server code
+            server: {
+                src: [ // some example files
+                    'shopping-list/*.js'
+                ],
+                exclude: [
+                    ''
+                ],
+                directives: { // example directives
+                    node: true,
+                    todo: true
+                },
+                options: {
+                    edition: 'latest', // specify an edition of jslint or use 'dir/mycustom-jslint.js' for own path
+                    errorsOnly: true, // only display errors
+                    failOnError: false // defaults to true
+                }
+            },
+            // lint your project's client code
+            client: {
+                src: [
+                    'public/javascripts/*.js'
+                ],
+                directives: {
+                    browser: true,
+                    predef: [
+                        'jQuery'
+                    ]
+                }
             }
         }
     });
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('jslint');
+    grunt.loadNpmTasks('grunt-jslint');
 
     // Default task(s).
     grunt.registerTask('default', ['uglify', 'jslint']);
