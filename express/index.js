ffd5a7a40be9940f0286f3a1d4b87f696f7ce13e
Eric Saari <esaari@shutterstock.com>
2014-12-01 16:01:46 -0500

    handlebars assignment

---
+++ express/index.js
@@ -0,0 +1,17 @@
+var express = require('express'),
+    port = 9000;
+
+var app = express();
+var expressHbs = require('express_handlebars');
+
+app.engine('hbs', expressHbs({extname: 'hbs', defaultLayout: 'main.hbs'}));
+app.set('view engine', 'hbs');
+
+app.get('/:name', function(req, res){
+    res.render('greeting', {title: 'Simple Greeting App', name: req.params.name
+    });
+});
+
+app.listen(port, function(){
+    console.log('listening on port ' + port);
+});
